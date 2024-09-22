887ca7da5ab06a860e720524d4b30ec965704ab6
Philip-Machar <philipmachar788@gmail.com>
2024-09-22 21:48:55 +0300

    Calculating sum of 1 - 100 using do while loop

---
+++ do_while_loop.cpp
@@ -0,0 +1,37 @@
+/*
+Author: Philip Machar
+Reg No: BSE-01-0047/2024
+Description: A program to display 1 - 100 and calculate the sum.
+*/
+
+//importing the input/output stream library
+#include <iostream>
+
+//Declaring the standard namespace for input/output
+using namespace std;
+
+//The main function where execution begins
+int main() {
+    
+    //Declaring variable i and sum
+    int i = 1, sum = 0;
+    
+    // Looping from 1 to 100
+    do {
+        
+        //Displaying the value of i in each iteration
+        cout << i << endl;
+        
+        //Adding the current value of i to sum
+        sum += i;
+        
+        //incrementing the value of i by 1
+        i++;
+    } while (i <= 100);
+    
+    // Displaying the final value of sum
+    cout << "The sum is: " << sum;
+    
+    //Telling the OS that our program was successfully executed
+    return 0;
+}
